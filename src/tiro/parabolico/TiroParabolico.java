9dd781e51f0965cd276bb5674651dd344b5ed8c1
manolo <manolosavi@me.com>
2014-02-24 11:25:46 -0600

    Initial commit.

---
+++ src/tiro/parabolico/TiroParabolico.java
@@ -0,0 +1,21 @@
+//
+//  Created by manolo on Feb 24, 2014.
+//  Copyright (c) 2014 manolo. All rights reserved.
+//
+
+package tiro.parabolico;
+
+/**
+ *
+ * @author manolo
+ */
+public class TiroParabolico {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+	
+}
